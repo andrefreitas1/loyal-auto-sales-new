864dcf5d4c91b20dced494cb195f06ff33698dda
Andre Freitas <andre@loyal.com>
2025-03-14 14:04:39 -0300

    fix: remove migrações problemáticas

--- prisma/migrations/add_last_name_field.sql
+++
@@ -1,8 +0,0 @@
--- Adiciona a coluna lastName
-ALTER TABLE "Customer" ADD COLUMN "lastName" TEXT;
-
--- Atualiza os registros existentes copiando o valor de firstName (temporariamente)
-UPDATE "Customer" SET "lastName" = "firstName" WHERE "lastName" IS NULL;
-
--- Torna a coluna lastName obrigatória
-ALTER TABLE "Customer" ALTER COLUMN "lastName" SET NOT NULL; 
